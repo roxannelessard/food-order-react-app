29cb188c2f5762343cd029efe9003830d86a9069
roxanne lessard <me@rlessard.com>
2022-01-04 16:00:04 -0500

    feat: adds meal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
