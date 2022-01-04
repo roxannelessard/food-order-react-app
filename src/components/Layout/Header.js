5ace2be63278e0a925227f4fa6cecd609fb1e135
roxanne lessard <me@rlessard.com>
2022-01-04 17:57:12 -0500

    fix: image and header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,15 +5,17 @@ import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
-  return <Fragment>
-    <header className={classes.header}>
-      <h1>ReactMeals</h1>
-      <HeaderCartButton/>
-    </header>
-    <div className={classes['main-image']}>
-      <img src={mealsImage} alt='Table with food'/>
-    </div>
-  </Fragment>
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table with food" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
